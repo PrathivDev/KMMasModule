259ad918b17b8e902f8104050ed876695a6890b0
“PrathivDev” <arprathiv7@gmail.com>
2024-09-06 12:50:54 +0530

    Moved publishing logic to maven central sonatype with package version 1.0.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,12 +1,15 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.compose.compiler)
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.28.0"
+
 }
 
 android {
-    namespace = "com.kobil.kmmasmodule.android"
+    namespace = "io.github.prathivdev"
     compileSdk = 34
     defaultConfig {
         minSdk = 24
@@ -49,15 +52,55 @@ dependencies {
 
 
 
-afterEvaluate {
-    publishing {
-        publications {
-            register<MavenPublication>("release") {
-                groupId = "com.github.Prathiv07"
-                artifactId = "KMMasModule"
-                version = "4.2"
-                from(components["release"])
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            register<MavenPublication>("release") {
+//                groupId = "com.github.Prathiv07"
+//                artifactId = "KMMasModule"
+//                version = "4.2"
+//                from(components["release"])
+//            }
+//        }
+//    }
+//}
+
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.prathivdev",
+        artifactId = "KMMasModule",
+        version = "1.0.0"
+    )
+    pom {
+        name.set("Sample KMM UI")
+        description.set("Sample KMM project as library includes both UI and shared Module")
+        inceptionYear.set("2024")
+        url.set("https://github.com/PrathivDev/KMMasModule")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
             }
         }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("io.github.prathivdev")
+                name.set("Prathiv A R")
+                email.set("arprathiv7@gmail.com")
+            }
+        }
+        scm {
+            url.set("https://github.com/PrathivDev/KMMasModule")
+        }
     }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,10 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.28.0"
+
 }
 
 kotlin {
@@ -27,7 +30,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -36,7 +39,7 @@ kotlin {
 }
 
 android {
-    namespace = "com.kobil.kmmasmodule"
+    namespace = "io.github.prathivdev"
     compileSdk = 34
     defaultConfig {
         minSdk = 24
@@ -47,16 +50,56 @@ android {
     }
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            register<MavenPublication>("release") {
-                groupId = "com.github.Prathiv07"
-                artifactId = "shared"
-                version = "4.2"
-                from(components["kotlin"])
-            }
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            register<MavenPublication>("release") {
+//                groupId = "com.github.Prathiv07"
+//                artifactId = "shared"
+//                version = "4.2"
+//                from(components["kotlin"])
+//            }
+//
+//        }
+//    }
+//}
 
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.prathivdev",
+        artifactId = "shared",
+        version = "1.0.0"
+    )
+    pom {
+        name.set("Sample KMM")
+        description.set("Sample KMM project as library includes both UI and shared Module")
+        inceptionYear.set("2024")
+        url.set("https://github.com/PrathivDev/KMMasModule")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("io.github.prathivdev")
+                name.set("Prathiv A R")
+                email.set("arprathiv7@gmail.com")
+            }
+        }
+        scm {
+            url.set("https://github.com/PrathivDev/KMMasModule")
         }
     }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
